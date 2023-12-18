410fc82d2fb92a22fa63b3e8509edd11124167c5
Maxim Voynov <maximm.14@yandex.ru>
2023-12-18 15:39:40 +0700

    Update ldap-auth.py

--- ldap-auth.py
+++ ldap-auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import sys
-sys.path.append( '/config/module/lib' )
+sys.path.append('{}/lib'.format(sys.path[0]))
 import os
+import json
 import argparse
 import configparser
 from ldap3 import Server, Connection, ALL, core
@@ -11,40 +12,46 @@ parser.add_argument('-m', '--meta', action='store_true', help='Enable meta to ou
 args = parser.parse_args()
 
 config = configparser.ConfigParser()
-config.read("/config/module/.env.ini")
+config.read("{}/.env.ini".format(sys.path[0]))
 
 if 'username' not in os.environ and 'password' not in os.environ:
-    print("Need username and password environment variables!")
+    print("# Username and password environment variables not set!")
     exit(1)
 
-config = configparser.ConfigParser()
-config.read("/config/module/.env.ini")
+username = os.environ['username']
+password = os.environ['password']
+
+SERVER = config['LDAP']['host']
+USERDN = config['LDAP']['userdn'].format(username)
+BASEDN = config['LDAP']['basedn']
+FILTER = config['LDAP']['filter'].format(username)
+ATTRIB = config['LDAP']['attrib']
+ISLOCAL = config['LDAP']['ISLOCAL']
 
 def main():
-    username = os.environ['username']
-    password = os.environ['password']
-    SERVER = config['LDAP']['host']
-    USERDN = config['LDAP']['userdn'].format(os.environ['username'])
-    BASEDN = config['LDAP']['basedn']
-    FILTER = config['LDAP']['filter'].format(os.environ['username'])
-    TIMEOUT = 3
-    #SCOPE = "base"
-    #ATTRS = ""
-
-    server = Server(SERVER, get_info=ALL)
+    s = Server(SERVER, get_info=ALL)
     try:
-        conn = Connection(server, USERDN, password=username, auto_bind=True)
         if not args.meta:
-            print("whoami: {}".format(conn.extend.standard.who_am_i()))
-        search = conn.search(BASEDN, FILTER)
+            print("# Trying authentication for user [{}]".format(username))
+
+        c = Connection(s, USERDN, password=password, auto_bind=True)
+        search = c.search(BASEDN, FILTER, attributes=ATTRIB)
+
         if search:
             if args.meta:
-                print("name = {}".format(username.capitalize()),"\ngroup = system-users")
-            print("Success LDAP: Search", conn.entries)
-            exit(0)
+                print("name={}".format(username.capitalize()))
+                print("group={}".format(json.loads(c.entries[0].entry_to_json())['attributes'][ATTRIB][0]))
+                print("local_only={}".format(ISLOCAL))
+                exit(0)
+            else:
+                print("# User [{}] successfully authenticated!".format(username))
+                exit(0)
         else:
-            print("Error LDAP: no access rights")
-            exit(1)
+            if args.meta:
+                exit(1)
+            else:
+                print("# User [{}] no access rights!".format(username))
+                exit(1)
     except core.exceptions.LDAPBindError as e:
         print(e)
         exit(1)
