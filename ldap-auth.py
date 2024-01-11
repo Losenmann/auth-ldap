eaafb661e75e62495310165b5eb5145143dec21e
Maxim Voynov <maximm.14@yandex.ru>
2024-01-11 14:25:50 +0700

    Update ldap-auth.py

--- ldap-auth.py
+++ ldap-auth.py
@@ -2,11 +2,12 @@
 import sys
 sys.path.append('{}/lib'.format(sys.path[0]))
 import os
+import json
 import hashlib
 import argparse
 import configparser
 from PIL import Image
-from ldap3 import Server, Connection, ALL, core
+from ldap3 import Server, Connection, SAFE_SYNC
 
 parser = argparse.ArgumentParser(description="Home Assistant LDAP authentication via CLI")
 parser.add_argument('-m', '--meta', action='store_true', help='Enable meta to output credentials to stdout')
@@ -34,11 +35,14 @@ else:
         exit(1)
 
 SERVER = config['LDAP']['host']
-USERDN = config['LDAP']['userdn']
-ISACTIVE = config['LDAP']['ISACTIVE']
-ISLOCAL = config['LDAP']['ISLOCAL']
+USERDN = config['LDAP']['userdn'].format(USER)
+BASEDN = config['LDAP']['basedn']
+FILTER = config['LDAP']['filter'].format(USER)
+ISACTIVE = config['LDAP']['isactive']
+ISLOCAL = config['LDAP']['islocal']
 
 def main():
+#    person()
     if not args.meta:
         print("# Trying authentication for user [{}]".format(USER))
 
@@ -58,27 +62,37 @@ def main():
         exit(0)
 
 def ldap():
-    s = Server(SERVER, get_info=ALL)
+    s = Server(SERVER)
     try:
-        rtn = [None] * 3
-        c = Connection(s, USERDN.format(USER), password=PASS, auto_bind=True)
+        rtn = [None] * 4
+        c = Connection(s, USERDN, PASS, client_strategy=SAFE_SYNC, auto_bind=True)
+        _, _, res, _ = c.search(BASEDN, FILTER, attributes=["uid","memberof","displayName","jpegPhoto"])
 
-        """ Get user """
-        user = c.search(config['LDAP']['basedn_user'].format(USER), config['LDAP']['filter_user'].format(USER), attributes=['givenName','jpegPhoto'])
-        if user:
-            rtn[0] = c.response[0].get('attributes', {}).get('givenName', '')[0]
-            rtn[2] = c.response[0].get('attributes', {}).get('jpegPhoto', '')[0]
+        if res[0].get('attributes').get('uid'):
+            rtn[0] = res[0].get('attributes').get('uid')[0]
         else:
             print("# User [{}] is not found!".format(USER))
             exit(1)
 
-        """ Get user group """
-        group = c.search(config['LDAP']['basedn_group'], config['LDAP']['filter_group'].format(USER), attributes=['cn'])
-        if group:
-            rtn[1] = c.response[0].get('attributes', {}).get('cn', '')[0]
+        if res[0].get('attributes').get('memberOf'):
+            for i in res[0].get('attributes').get('memberOf'):
+                match i[:i.find('cn=homeassistant')+16]:
+                    case 'cn=system-users,cn=homeassistant' | 'cn=homeassistant':
+                        rtn[1] = 'system-users'
+                    case 'cn=system-admin,cn=homeassistant':
+                        rtn[1] = 'system-admin'
+                        break
         else:
             print("# User [{}] no access rights!".format(USER))
             exit(1)
+
+        if res[0].get('attributes').get('displayName'):
+            rtn[2] = res[0].get('attributes').get('displayName')
+        else:
+            rtn[2] = res[0].get('attributes').get('uid')[0]
+
+        if res[0].get('attributes').get('jpegPhoto'):
+            rtn[3] = res[0].get('attributes').get('jpegPhoto')[0]
 
         return rtn
     except core.exceptions.LDAPBindError as e:
@@ -101,6 +115,29 @@ def photo(img):
     hsize = int((float(i.size[1]) * float(wpercent)))
     i = i.resize((512, hsize), Image.Resampling.LANCZOS)
     i.save("/config/image/{}/512x512".format(n), "JPEG")
+
+def person(i=None):
+    if i is None:
+        i = "/api/image/serve/" + hashlib.md5(os.urandom(32)).hexdigest() + "/512x512"
+    p = {"id": "usertest","name":"test","user_id":"","device_trackers":[],"user_id":"null","picture":i}
+    print(p)
+################
+#    async def async_create_person(hass, name, *, user_id=None, device_trackers=None):
+#        """Create a new person."""
+#        await hass.data[DOMAIN][1].async_create_item(
+#            {
+#                ATTR_NAME: "usertestq",
+#                ATTR_USER_ID: "usertest1",
+#                CONF_DEVICE_TRACKERS: device_trackers or [],
+#            }
+#        )
+
+#    with open('/config/advanced/module/data.json', 'r+') as f:
+#        d = json.load(f)
+#        d['data']['items'].append(p)
+#        f.seek(0)
+#        json.dump(d, f, indent=4)
+#        f.truncate()
 
 if __name__ == "__main__":
     main()
